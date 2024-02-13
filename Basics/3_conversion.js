90d76826a26c0bd938259e135f4f558418c70db1
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-13 09:42:56 +0500

    Create 3_conversion.js

---
+++ Basics/3_conversion.js
@@ -0,0 +1,46 @@
+// conversion in javascript
+
+// variable
+let score = 50;
+// converting number string
+let stringNumber = String(score);
+
+console.log(stringNumber);
+// checking type of variable
+console.log(typeof stringNumber);
+
+// converting string to number
+let valueof = "50";
+
+// converting string into number
+let Numbervalueof = Number(valueof);
+
+console.log(Numbervalueof);
+
+console.log(typeof Numbervalueof);
+
+let value = "50AbC";
+// converting string to number when number is mixed with string
+let Numbervalue = Number(value);
+
+console.log(Numbervalue);
+console.log(typeof Numbervalue);
+
+let i = 1;
+// converting 0 and 1 in boolean
+let booleani = Boolean(i);
+
+console.log(booleani);
+
+console.log(typeof booleani);
+
+//0 => false
+//1 => true
+
+let a = "";
+//converting string into boolean
+let booleana = Boolean(a);
+
+console.log(booleana);
+// "" empty string => false
+console.log(typeof booleana);
