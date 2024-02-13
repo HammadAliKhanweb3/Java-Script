2e44b4e377020fd3843c2dadafbe3c0a92c2b6bc
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-13 09:52:59 +0500

    Create operations.js

---
+++ Basics/operations.js
@@ -0,0 +1,33 @@
+// operations in javaScript
+
+// variable
+let value = 50;
+
+// assigning negative value
+let negvalue = -value;
+
+console.log(negvalue);
+
+console.log("1" + 6);
+console.log(1 + "6");
+console.log("1" + 6 + 6);
+console.log(1 + 6 + 6);
+
+// recommended for code readibility to use braces for many operations
+console.log(((2 + 6) * 1) % 6);
+
+// 1 will show on output
+console.log(+true);
+
+// throws error
+// console.log(true+);
+
+//pre fix
+let counter = 100;
+++counter;
+console.log(counter);
+
+//post fix
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
