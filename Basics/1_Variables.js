4cde37e9d797274bbcd0fc847dcf5190fa2cea77
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-13 09:35:05 +0500

    Create 1_Variables.js
    Variables in JavaScript

---
+++ Basics/1_Variables.js
@@ -0,0 +1,19 @@
+// Variables are memory container used to stor data in Memory OR variables are named memory location
+// 4 Ways to declare variable in javaScipt
+
+// Can't be change again when we use const 
+const Name = "Hammad";
+
+let Age = 19;
+
+var Height = 6;
+// Prefer not to use Var
+// Beacuse of issue in block scope and functional scope
+
+address = "Pakistan";
+ 
+// print values in the form of table 
+console.table([Name, Age, Height]);
+//  Knowing the datatype of variable
+console.log(typeof Name);
+
