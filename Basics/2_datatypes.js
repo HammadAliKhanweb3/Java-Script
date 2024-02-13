42d4278b0daf3dbfac35beffd5fc2873d51f4af6
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-13 09:38:58 +0500

    Create 2_datatypes.js

---
+++ Basics/2_datatypes.js
@@ -0,0 +1,24 @@
+"use strict ";    //tereats all js code as newer version
+ alert(5+5)   // we are using in node not browser so it will throw error
+ // string
+ let Name="Hammad"     
+// number
+ let Age=19             
+// Boolean
+ let isLoggedin=true    
+// we can also declare variable with keyword but it is not good practice
+ userId=51              
+ console.log(userID);
+ 
+ 
+ /* Prem
+ number => 2 to power 53 (no of data can store)
+ Bigint  => (for heavy data )
+ string =>  {we write string in double coutation ("")}
+ Boolean =>  {Boolean will give only true/false}
+ null    =>  standaloneValue
+ undefined 
+ symbol  =>  unique
+ 
+ 
+ */
