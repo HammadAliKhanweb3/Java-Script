b0d61e322b24a41e326a1da4aeb913218f44934c
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-14 14:47:11 +0500

    Create 7_numbers.js

---
+++ Basics/7_numbers.js
@@ -0,0 +1,21 @@
+// numbers in javascript
+
+// variable of number datatype
+const num = 500.123;
+
+const otherNum = new Number(200);
+
+console.log(otherNum);
+
+let number = 1.1214;
+
+// converting number into exponent
+let n = num.toExponential(3);
+console.log(n);
+
+// we can convert number to string and can use the properties of strings
+// converting number into string
+let v = num.toString().length;
+console.log(v);
+
+console.log(num.toFixed(2));
