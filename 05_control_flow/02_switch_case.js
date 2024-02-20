f06c52680c021fb980045ecec7e4e43353e7bfb5
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-20 09:34:25 +0500

    switch case in java script

---
+++ 05_control_flow/02_switch_case.js
@@ -0,0 +1,27 @@
+// switch case in javaScript
+
+// variable
+let month = "march";
+
+// switch case condition to check the month
+switch (month) {
+  case "january":
+    console.log("month is january");
+    break;
+  case "febraury":
+    console.log("month is febraury");
+    break;
+  case "march":
+    console.log("month is march");
+    break;
+  case "april":
+     console.log("month is april");
+    break;
+  case "may":
+    console.log("month is may");
+    break;
+  case "june":
+    console.log("month is june");
+    break;
+  
+}
