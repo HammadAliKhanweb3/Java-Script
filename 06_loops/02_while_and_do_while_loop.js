21a89f76f5c9f797f3a82a4bfa1993320266daf6
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-21 11:22:17 +0500

    Create 02_while_and_do_while_loop.js

---
+++ 06_loops/02_while_and_do_while_loop.js
@@ -0,0 +1,31 @@
+// while loop in javascript
+
+// variable
+let index = 0;
+
+// while loop
+while (index < 10) {
+  console.log(`value of index is ${index}`);
+  index = index + 2;
+}
+
+// array
+let myArray = ["Superman", "Batman", "Flash"];
+let i = 0;
+
+// while loop to print elements in array
+while (i < myArray.length) {
+  let element = myArray[i];
+  console.log(element);
+  i++;
+}
+
+let j = 0;
+
+// do while loop
+do {
+  console.log(`${j} is going`);
+  j++;
+} while (j < 10);
+{
+}
