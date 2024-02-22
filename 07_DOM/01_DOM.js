d6491ffbfed0d81f02e578f12dd855efcec65dd2
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-02-22 12:29:36 +0500

    DOM in java script

---
+++ 07_DOM/01_DOM.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DOM Learnig</title>
+    <style>
+      .title {
+        background-color: #212121;
+        color: aliceblue;
+      }
+    </style>
+  </head>
+  <body class="title">
+    <div class="container">
+      <h1 id="first heading">
+        Learning DOM for the first time
+        <span style="display: none">hi</span>
+      </h1>
+      <h2>Hammad</h2>
+      <h2>Ali</h2>
+      <h2>Khan</h2>
+      <h2>Faraz</h2>
+      <input type="password" />
+      <p>Enjoying javascript</p>
+    </div>
+    <ul>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+      <li>four</li>
+      <li>five</li>
+    </ul>
+  </body> 
+</html>
